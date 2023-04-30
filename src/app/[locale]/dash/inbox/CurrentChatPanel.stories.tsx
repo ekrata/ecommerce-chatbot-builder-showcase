25d2fd47937ebdc77394792d99704eddb3eefbf3
Luke Cowell <luke@ekrata.com>
2023-05-01 09:56:47 +1000

    fix: update lintstaged

--- src/app/[locale]/dash/inbox/CurrentChatPanel.stories.tsx
+++ src/app/[locale]/dash/inbox/CurrentChatPanel.stories.tsx
@@ -1,6 +1,5 @@
 import { expect } from '@storybook/jest';
-import type { Meta, StoryObj } from '@storybook/react';
-import { within, userEvent } from '@storybook/testing-library';
+
 import { ChatListPanel } from './ChatListPanel';
 import { createRandomChat } from './mocks.test';
 import ChatLog from './ChatLog';
